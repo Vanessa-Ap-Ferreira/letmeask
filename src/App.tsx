2d8550fe972b502a9763f2f28b8033981f07814f
Vanessa Ferreira <vanessaf689@gmail.com>
2022-05-23 16:28:38 -0300

    Página admin, like questão, remover sala

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/AdminRoom";
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AuthContextProvider } from "./context/AuthContext";
@@ -11,9 +12,10 @@ function App() {
     <BrowserRouter>      
       <AuthContextProvider>
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/rooms/new" element={<NewRoom />} />
-            <Route path="/rooms/:id" element={<Room />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms/new" element={<NewRoom />} />
+          <Route path="/rooms/:id" element={<Room />} />
+          <Route path="/admin/rooms/:id" element={<AdminRoom />} />
         </Routes> 
       </AuthContextProvider>        
     </BrowserRouter>
